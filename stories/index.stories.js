80b18a2dfb6129a418b637ac0ce86dc47b4bfc2e
takupeso <takuma.abe4002@gmail.com>
2019-02-21 16:06:33 +0900

    add storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,6 +6,8 @@ import { linkTo } from '@storybook/addon-links';
 
 import MyButton from './atoms/MyButton';
 import Welcome from './molecules/Welcome';
+import PortfolioCard from '~/components/molecules/PortfolioCard';
+import 'bulma/css/bulma.css'
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
   components: { Welcome },
@@ -33,5 +35,28 @@ storiesOf('Button', module)
     methods: { action: action('clicked') },
   }));
 
+  storiesOf('PortfolioCard', module).add('Storybook', () => ({
+    components: { PortfolioCard },
+    data() {
+      return {
+        portfolio:
+        {"uuid":"portxxxxx1",
+        "site_url":"https://site-example.com/1",
+        "image_urls":["https://images.unsplash.com/photo-1475778057357-d35f37fa89dd","https://images.unsplash.com/photo-1475778057357-d35f37fa89dd","https://images.unsplash.com/photo-1475778057357-d35f37fa89dd"],
+        "portfolio_comment":"俺の渾身のポートフォリオサイト、みんな見てくれよな！",
+        "comments":[{"myself":true,"comment":"私のコメント"},{"myself":false,"comment":"誰かのコメント"}],
+        "corrections":[{"myself":true,"correction":"添削への返信"},{"myself":false,"correction":"誰かの添削コメント"}],
+        "like":4,
+        "user":
+          {"name":"john",
+          "position":["フロントエンド","バックエンド","デザイナー"],
+          "status":"戦闘中",
+          "user_small_images_url":"https://bulma.io/images/placeholders/96x96.png"}
+        }
+      }
+    },
+    template: '<div style="width: 500px; "><portfolio-card :portfolio="portfolio" /></div>',
+    methods: { action: linkTo('Button') },
+  }));
 
 /* eslint-enable react/react-in-jsx-scope */

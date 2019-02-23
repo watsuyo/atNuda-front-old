c7d13d085e62e9fed09b5b553ca3318ca0eb4865
takupeso <takuma.abe4002@gmail.com>
2019-02-23 16:48:11 +0900

    remove storybook

--- pages/top.vue
+++ pages/top.vue
@@ -38,52 +38,18 @@
       </nav>
     </header>
     <main>
-      <section>
-        <div class="columns mt-50">
-          <div class="column is-10 is-offset-1">
-            <div class="lavel">
-              <div class="level-left">
-                <div class="level-item is-3">
-                  <div class="control has-icons-left">
-                    <div class="select">
-                      <select>
-                        <option selected>ポジション</option>
-                      </select>
-                    </div>
-                    <span class="icon is-left">
-                      <i class="fas fa-globe"></i>
-                    </span>
-                  </div>
-                </div>
-                <div class="level-item is-3">
-                  <div class="control has-icons-left">
-                    <div class="select">
-                      <select>
-                        <option selected>ステータス</option>
-                      </select>
-                    </div>
-                    <span class="icon is-left">
-                      <i class="fas fa-globe"></i>
-                    </span>
-                  </div>
-                </div>
-                <div>のポートフォリオ</div>
-              </div>
-            </div>
+      <section class="section">
+        <portfolio-search-box></portfolio-search-box>
+      </section>
+      <section class="section">
+        <div class="column">
+          <div class="buttons is-centered">
+            <a class="button">新着順</a>
+            <a class="button">人気順</a>
           </div>
         </div>
       </section>
-      <section class="section is-medium has-background-warning mt-50">
-        <div class="columns has-background-white">
-          <div class="column is-5"></div>
-          <div class="column">
-            <div class="buttons is-centered">
-              <a class="button">新着順</a>
-              <a class="button">人気順</a>
-            </div>
-          </div>
-          <div class="column is-5"></div>
-        </div>
+      <section class="section">
         <div class="columns has-background-white">
           <div class="column is-4 is-offset-1">
             <portfolio-card v-bind:portfolio="portfolio"></portfolio-card>
@@ -141,6 +107,7 @@
 
 <script>
 import PortfolioCard from "../components/molecules/PortfolioCard"
+import PortfolioSearchBox from "../components/organisms/PortfolioSearchBox"
 
 export default {
   data () {
@@ -162,7 +129,8 @@ export default {
     }
   },
   components: {
-    "portfolio-card": PortfolioCard
+    "portfolio-card": PortfolioCard,
+    "portfolio-search-box": PortfolioSearchBox
   }
 }
 </script>
